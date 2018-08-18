b257d0c4a6aee7d599f2e494109fe6449bc17f58
adis2hope <40731640+adis2hope@users.noreply.github.com>
2018-08-18 13:35:54 +0530

    Update DomainCreation_Properties.py

--- DomainCreation_Properties.py
+++ DomainCreation_Properties.py
@@ -13,10 +13,9 @@ def printInStyle(txt):
 # Create a domain from the weblogic domain template.
 #==========================================
 print(WLHOME)
-readTemplate("/u01/app/software/mw_home/fmw/oraclehome/wlserver/common/templates/wls/wls.jar")
-dumpStack()
-#selectTemplate(WLHOME+'/common/templates/wls/wls.jar')
-#loadTemplates()
+#readTemplate("/u01/app/software/mw_home/fmw/oraclehome/wlserver/common/templates/wls/wls.jar")
+selectTemplate(WLHOME+'/common/templates/wls/wls.jar','12.2.1.3')
+loadTemplates()
 printInStyle('Setting AdminServer name')
 cd('Servers/AdminServer')
 set('Name',adminServerName)
@@ -25,7 +24,7 @@ set('Name',adminServerName)
 #==========================================
 printInStyle('Set the ListenAddress and ListenPort')
 set('ListenAddress',adminServerListenaddr)
-set('ListenPort', admlistenport)
+set('ListenPort', int(admlistenport))
 #====================================================
 # Define the password for user weblogic. You must define the password before you
 # can write the domain.
