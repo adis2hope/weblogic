74233b293054da41a4c146bedca1c9021cd2bfe9
adis2hope <40731640+adis2hope@users.noreply.github.com>
2018-08-18 12:32:09 +0530

    Create DomainCreation_Properties.py

---
+++ DomainCreation_Properties.py
@@ -0,0 +1,49 @@
+###########################################################
+# This script will dynamically create the domain as per your inputs
+###########################################################
+import os
+WLHOME=os.environ['WL_HOME']
+domainLoc=os.environ['MW_HOME']
+loadProperties('labWLSTdom.properties')
+
+def printInStyle(txt):
+ print'-'*10 +txt
+ 
+#==========================================
+# Create a domain from the weblogic domain template.
+#==========================================
+print(WLHOME)
+readTemplate(WLHOME+'/common/templates/wls/wls.jar')
+printInStyle('Setting AdminServer name')
+cd('Servers/AdminServer')
+set('Name',adminServerName)
+#==========================================
+# Configure the Administration Server
+#==========================================
+printInStyle('Set the ListenAddress and ListenPort')
+set('ListenAddress',adminServerListenaddr)
+set('ListenPort', admlistenport)
+#====================================================
+# Define the password for user weblogic. You must define the password before you
+# can write the domain.
+#====================================================
+printInStyle('Creating Password')
+cd('/')
+cd('Security/base_domain/User/weblogic')
+set('Name',adminUser)
+cmo.setPassword(adminPassword)
+
+printInStyle('Setting StartUp Options')
+setOption('OverwriteDomain', 'true')
+#==============================================
+# Write the domain, close template and finally exit from the WLST
+#==============================================
+print 'Given domain path, name : ', domainLoc, domainName
+# Create Domain to File System
+printInStyle('Writing Domain To File System')
+writeDomain(domainLoc+"/"+domainName)
+closeTemplate()
+
+# Exiting
+print('Exiting now...')
+exit()
